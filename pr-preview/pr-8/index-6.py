78441d7235b930440f787af9dad0ee254316c617
tkoyama010 <tkoyama010@users.noreply.github.com>
2024-01-20 09:07:16 +0000

    Deploy preview for PR 8 🛫

--- pr-preview/pr-8/index-6.py
+++ pr-preview/pr-8/index-6.py
@@ -1,6 +1,16 @@
-import pyvista
+from itertools import product
+import pyvista as pv
+from pyvista import examples
 
-from pyvista import demos
-pl = demos.orientation_plotter()
-pl.camera_position = 'yz'
-pl.show()
+cubemap = examples.download_sky_box_cube_map()
+colors = ['red', 'teal', 'black', 'orange', 'silver']
+
+p = pv.Plotter()
+p.set_environment_texture(cubemap)
+
+for i, j in product(range(5), range(6)):
+    sphere = pv.Sphere(radius=0.5, center=(0.0, 4 - i, j))
+    p.add_mesh(sphere, color=colors[i], pbr=True, metallic=i / 4, roughness=j / 5)
+
+p.view_vector((-1, 0, 0), (0, 1, 0))
+p.show()
