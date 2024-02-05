86030e7f789fc7d12f0aabfe2470e508ee8cfc6a
tkoyama010 <tkoyama010@users.noreply.github.com>
2024-02-05 00:13:33 +0000

    Deploy preview for PR 8 🛫

--- pr-preview/pr-8/index-3.py
+++ pr-preview/pr-8/index-3.py
@@ -1,8 +1,17 @@
 import pyvista as pv
+import numpy as np
 from pyvista import examples
 
-mesh = pv.Sphere()
-tex = examples.download_masonry_texture()
+texture = examples.load_globe_texture()
 
-mesh.texture_map_to_sphere(inplace=True)
-mesh.plot(texture=tex)
+sphere = pv.Sphere(
+    radius=1, theta_resolution=120, phi_resolution=120, start_theta=270.001, end_theta=270
+)
+
+sphere.active_texture_coordinates = np.zeros((sphere.points.shape[0], 2))
+for i in range(sphere.points.shape[0]):
+    sphere.active_texture_coordinates[i] = [
+        0.5 + np.arctan2(-sphere.points[i, 0], sphere.points[i, 1]) / (2 * np.pi),
+        0.5 + np.arcsin(sphere.points[i, 2]) / np.pi,
+    ]
+sphere.plot(texture=texture)
