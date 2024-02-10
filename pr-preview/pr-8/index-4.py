b0f8f8d1936030cd44b5f460aed81d30c124c0b0
tkoyama010 <tkoyama010@users.noreply.github.com>
2024-02-10 05:51:12 +0000

    Deploy preview for PR 8 🛫

--- pr-preview/pr-8/index-4.py
+++ pr-preview/pr-8/index-4.py
@@ -2,10 +2,6 @@ import pyvista as pv
 from pyvista import examples
 
 pl = pv.Plotter()
-image_path = examples.planets.download_stars_sky_background(
-    load=False
-)
-pl.add_background_image(image_path)
 mesh = examples.planets.load_moon()
 texture = examples.planets.download_moon_surface(texture=True)
 _ = pl.add_mesh(mesh, texture=texture)
