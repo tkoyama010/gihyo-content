d231ff30e7912a0e86b09abb1e981220dc2b59e3
tkoyama010 <tkoyama010@users.noreply.github.com>
2024-07-19 04:15:18 +0000

    Remove preview for PR 43 🛬

--- pr-preview/pr-43/index-4.py
+++
@@ -1,8 +0,0 @@
-import pyvista as pv
-from pyvista import examples
-
-pl = pv.Plotter()
-mesh = examples.planets.load_moon()
-texture = examples.planets.download_moon_surface(texture=True)
-_ = pl.add_mesh(mesh, texture=texture)
-pl.show()
